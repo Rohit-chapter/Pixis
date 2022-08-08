1081fa986b7ffca1c57377fadab8c2306ed1762f
ashaytiwari <ashaytiwari2001@gmail.com>
2022-08-08 12:32:10 +0530

    initial setup for project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div id={styles.pixisAppMain}>
+      <h3>Pixis application</h3>
+    </div>
+  );
+}
+
+export default App;
